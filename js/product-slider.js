c5a9cb68d7454b05b357e2b33dca1493ab6717fa
eggrevolver <eggrevolver@gmail.com>
2017-10-17 21:37:32 +0300

    Implemented text movement (unfinished) Implemented magnifier (unfinished)

--- js/product-slider.js
+++ js/product-slider.js
@@ -1,8 +1,10 @@
 "use strict";
 
 var imageElement = document.querySelector(".item-image");
+var textElement = document.querySelector(".scrolling-text");
 var leftArrow = document.querySelector(".scroll-image-left");
 var rightArrow = document.querySelector(".scroll-image-right");
+var overlayElement = document.querySelector(".magnifier-overlay");
 
 var imagesArray = ["images/product/1.jpg",
     "images/product/2.jpg",
@@ -20,10 +22,12 @@ function setNextImage() {
         ++currentImageIndex;
     }
 
+    textElement.classList.add('moving-left');
     imageElement.classList.add('fadeOut');
 
     setTimeout(function () {
         imageElement.src = imagesArray[currentImageIndex];
+        overlayElement.style.backgroundImage = "url('" + imagesArray[currentImageIndex] + "')";
         imageElement.classList.remove('fadeOut');
         imageElement.classList.add('fadeIn');
     }, 100);
@@ -42,10 +46,25 @@ function setPreviousImage() {
 
     setTimeout(function () {
         imageElement.src = imagesArray[currentImageIndex];
+        overlayElement.style.backgroundImage = "url('" + imagesArray[currentImageIndex] + "')";
         imageElement.classList.remove('fadeOut');
         imageElement.classList.add('fadeIn');
     }, 100);
 }
 
+function zoomIn(event) {
+    overlayElement.style.display = "inline-block";
+    var posX = event.offsetX ? (event.offsetX) : event.pageX - imageElement.offsetLeft;
+    var posY = event.offsetY ? (event.offsetY) : event.pageY - imageElement.offsetTop;
+    overlayElement.style.backgroundPosition = (-posX * 4) + "px " + (-posY * 4) + "px";
+}
+
+function zoomOut() {
+    overlayElement.style.display = "none";
+}
+
 leftArrow.addEventListener('click', setPreviousImage);
 rightArrow.addEventListener('click', setNextImage);
+
+imageElement.addEventListener('mousemove', zoomIn);
+imageElement.addEventListener('mouseout', zoomOut);
